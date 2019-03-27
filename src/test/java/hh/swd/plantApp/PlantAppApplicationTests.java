a82b8461ef24874c199fa67123abbf8acf8801a8
Petra Kohva <petra.kohva@myy.haaga-helia.fi>
2019-03-27 10:48:21 +0200

    Projekti aloitettu

---
+++ src/test/java/hh/swd/plantApp/PlantAppApplicationTests.java
@@ -0,0 +1,16 @@
+package hh.swd.plantApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlantAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
